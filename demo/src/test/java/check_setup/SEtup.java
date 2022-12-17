6fbe6ac320c465ef2b8b78ced61666ccfc836c09
Pravinjd <pravin.j2295@gmail.com>
2022-12-18 00:51:50 +0530

    first commit

---
+++ demo/src/test/java/check_setup/SEtup.java
@@ -0,0 +1,13 @@
+package check_setup;
+
+import org.testng.annotations.Test;
+
+public class SEtup {
+	
+	
+  @Test
+  public void MyProgramisRuningornot() 
+  {
+	  System.out.println("my setup is properlry done");
+  }
+}
